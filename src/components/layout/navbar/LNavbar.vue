c103c8098dc3bb42bf96c5a0dcb817194f58f187
Fhaladin-Dev <farhan.nif.alaudin@gmail.com>
2025-02-19 19:35:51 +0700

    refactor navbar layout, enhance menu structure, and update PlaygroundView with UI components

--- src/components/layout/navbar/LNavbar.vue
+++ src/components/layout/navbar/LNavbar.vue
@@ -1,23 +1,25 @@
 <template>
   <header>
     <nav class="navbar mb-4">
-      <div class="navbar__left">
-        <div class="navbar__logo">
-          <RouterLink to="/"> fhaladin-dev </RouterLink>
-        </div>
-
-        <LNavbarMenu
-          v-for="(menu, index) in DNavbarMenu"
-          :key="menu.text"
-          :to="menu.to"
-          :class="{ last: index === DNavbarMenu.length - 1 }"
-        >
-          {{ menu.text }}
-        </LNavbarMenu>
+      <div class="navbar__logo">
+        <RouterLink to="/"> fhaladin-dev </RouterLink>
       </div>
 
-      <div class="navbar__contact-me">
-        <LNavbarMenu to="/contact-me">_contact-me</LNavbarMenu>
+      <div class="navbar__menu">
+        <div class="navbar__menu-left">
+          <LNavbarMenu
+            v-for="(menu, index) in DNavbarMenu"
+            :key="menu.text"
+            :to="menu.to"
+            :class="{ last: index === DNavbarMenu.length - 1 }"
+          >
+            {{ menu.text }}
+          </LNavbarMenu>
+        </div>
+
+        <div class="navbar__contact-me">
+          <LNavbarMenu to="/contact-me">_contact-me</LNavbarMenu>
+        </div>
       </div>
     </nav>
   </header>
--- src/views/PlaygroundView.vue
+++ src/views/PlaygroundView.vue
@@ -1,5 +1,54 @@
 <template>
-  <div class="about">
-    <h1 class="text-heading text-white">This is an playground page</h1>
+  <div>
+    <p class="text-black">Black</p>
+    <p class="text-midnight-navy">Midnight Navy</p>
+    <p class="text-midnight-teal">Midnight Teal</p>
+    <p class="text-charcoal-blue">Charcoal Blue</p>
+
+    <p class="text-steel-blue">Steel Blue</p>
+    <p class="text-periwinkle-blue">Periwinkle Blue</p>
+
+    <p class="text-tropical-teal">Tropical Teal</p>
+    <p class="text-mint-green">Mint Green</p>
+
+    <p class="text-sunset-orange">Sunset Orange</p>
+    <p class="text-coral-pink">Coral Pink</p>
+    <p class="text-lavender-purple">Lavender Purple</p>
+
+    <br />
+    <p class="text-sunset-orange text-heading">Typography Heading</p>
+    <p class="text-sunset-orange text-subheading">Typography Subheading</p>
+    <p class="text-sunset-orange text-body">Typography Body</p>
+    <p class="text-sunset-orange text-label">Typography Label</p>
+    <p class="text-sunset-orange text-code-snippets">Typography Code Snippets</p>
+    <br />
+
+    <UiButton variant="primary">start-game</UiButton>
+    <UiButton variant="secondary">start-game</UiButton>
+    <UiButton variant="outline">start-game</UiButton>
+
+    <UiIcon name="admin" variant="line" />
+    <UiIcon name="html5" />
+    <UiIcon name="arrow-right" variant="line" small />
+    <UiIcon name="store" />
+
+    <br />
+    <br />
+
+    <UiIconBox name="reactjs" color="text-steel-blue" class="bg-mint-green" />
+    <UiIconBox name="html5" class="bg-sunset-orange" />
+
+    <br />
+    <br />
+
+    <UiCard
+      image="/thumbnail/tb-01.png"
+      text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+      buttonText="view-project"
+      icon="html5"
+      iconBackgroundColor="bg-sunset-orange"
+    />
   </div>
 </template>
+
+<script setup lang="ts"></script>
