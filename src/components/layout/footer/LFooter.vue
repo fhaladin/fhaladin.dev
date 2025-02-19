98d1c7eca2e26281337762c437b2dab397f40c43
Fhaladin-Dev <farhan.nif.alaudin@gmail.com>
2025-02-19 20:25:41 +0700

    add footer component and styles, update layout to include footer

---
+++ src/components/layout/footer/LFooter.vue
@@ -0,0 +1,32 @@
+<template>
+  <footer class="footer">
+    <div class="footer__left">
+      <div class="footer__item border-r">find me in:</div>
+      <div
+        class="footer__item border-r px-4!"
+        @click="redirectUrl('https://www.instagram.com/__fhaladin/')"
+      >
+        <UiIcon name="instagram" small />
+      </div>
+      <div
+        class="footer__item border-r px-4!"
+        @click="redirectUrl('https://www.linkedin.com/in/farhan-hanif-alaudin-11412a139/')"
+      >
+        <UiIcon name="linkedin-box" small />
+      </div>
+    </div>
+
+    <div class="footer__right">
+      <div class="footer__item border-l" @click="redirectUrl('https://fhaladin-dev.medium.com')">
+        <span class="mr-2">fhaladin-dev</span>
+        <UiIcon name="medium" small />
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script setup lang="ts">
+const redirectUrl = (url: string) => {
+  window.open(url, '_blank')
+}
+</script>
--- src/layout/LayoutDefault.vue
+++ src/layout/LayoutDefault.vue
@@ -1,15 +1,19 @@
 <template>
-  <main>
-    <LNavbar />
+  <LNavbar />
 
+  <main class="h-[100vh] pt-[48px] pb-[53px] flex justify-center items-center">
     <RouterView v-slot="{ Component }">
       <Transition name="page" mode="out-in">
         <component :is="Component" />
       </Transition>
     </RouterView>
+
+    <LFooter />
   </main>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import LFooter from '@/components/layout/footer/LFooter.vue'
+</script>
 
 <style scoped></style>
